ff3c29fef33e51c5b0b900ef15dadc79651b87b7
ziqun zhou <zhouziqun@cool2645.com>
2017-03-27 23:22:54 +0800

    Fix boom 2.

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,10 +1,23 @@
 @extends('layouts.master')
-@section('nav')
-@endsection
 @section('content')
     <div id="layout" class="pure-g">
+        <div class="sidebar pure-u-1 pure-u-md-1-4">
+            <div class="header">
+                <h1 class="brand-title">南條愛乃の</h1>
+                <h2 class="brand-tagline">ツイッターロボット</h2>
 
-
+                <nav class="nav">
+                    <ul class="nav-list">
+                        <li class="nav-item">
+                            <a class="pure-button" href="http://purecss.io">Pure</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="pure-button" href="http://yuilibrary.com">YUI Library</a>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
+        </div>
         <div class="content pure-u-1 pure-u-md-3-4">
             <div>
                 <!-- A wrapper for all the blog posts -->
@@ -14,7 +27,7 @@
 
                         <section class="post">
                             <header class="post-header">
-                                <h2 class="post-title">Tweet</h2>
+                                <h2 class="post-title">{{$tweet->id}}</h2>
 
                             </header>
 
@@ -42,4 +55,3 @@
     </div>
 
 @endsection
-@section('footer', 'testfooter')
--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -8,26 +8,22 @@
     <!-- Compiled and minified CSS -->
     <link rel="stylesheet" href="https://unpkg.com/purecss@0.6.2/build/pure-min.css" integrity="sha384-" crossorigin="anonymous">
 
+    <link rel="stylesheet" href="https://unpkg.com/purecss@0.6.2/build/grids-responsive-min.css">
+
     <link rel="stylesheet" href="/css/app.css">
 
     <!--Let browser know website is optimized for mobile-->
     <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
 
-    <meta name="theme-color" content="#00ffff"/>
+    <meta name="theme-color" content="#00d6fe"/>
 
     <title>南條愛乃のツイッターロボット</title>
 </head>
 <body>
 
 <!--Import jQuery before materialize.js-->
-<script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
 
-<script type="text/javascript" src="/js/app.js"></script>
-
-@yield('nav')
 
 @yield('content')
-
-@yield('footer')
 </body>
 </html>
