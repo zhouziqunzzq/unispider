012b57aa56d92b43d2158ef86ece85a3ca740dd2
LI JIAHAO <lijiahao99131@gmail.com>
2017-04-16 20:52:58 +0800

    rebase nanjolno front-end

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,7 +1,66 @@
 @extends('layouts.master')
 @section('content')
+    <div id="layout" class="pure-g">
+        <div class="sidebar pure-u-1 pure-u-md-1-4">
+            <div class="header">
+                <h1 class="brand-title">南條愛乃の</h1>
+                <h2 class="brand-tagline">ツイッターロボット</h2>
 
-    <h1>Welcome using Unispider!</h1>
-    <a href="https://github.com/zhouziqunzzq/unispider">View it on Github</a>
+                <nav class="nav">
+                    <ul class="nav-list">
+                        <li class="nav-item">
+                            <a class="pure-button" href="http://purecss.io">Pure</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="pure-button" href="http://yuilibrary.com">YUI Library</a>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
+        </div>
+        <div class="content pure-u-1 pure-u-md-3-4">
+            <div>
+                <!-- A wrapper for all the blog posts -->
+                <div class="posts">
+                    @foreach($tweets as $tweet)
+                        <h1 class="content-subhead">{{date("Y-m-d H:i:s",$tweet->origin_created_at)}}</h1>
+
+                        <section class="post">
+                            <header class="post-header">
+                                <img width="48" height="48" alt="avatar" class="post-avatar" src="/img/n.jpg">
+                                <h2 class="post-title">{{$tweet->id}}</h2>
+                            </header>
+
+                            <div class="post-description">
+                                <p>
+                                    {!! empty($tweet->html_content) ? $tweet->text : $tweet->html_content !!}
+                                </p>
+                            </div>
+                            @if($tweet->trans_zh_author)
+                                <div class="post-description">
+                                    <p>
+                                        {{$tweet->trans_zh}}
+                                    </p>
+                                    <label class="content-subhead">翻译自：{{$tweet->trans_zh_author}}</label>
+                                </div>
+                            @endif
+                        </section>
+                    @endforeach
+
+                </div>
+
+                <div class="footer">
+                    <div class="pure-menu pure-menu-horizontal">
+                        <ul>
+                            <li class="pure-menu-item"><a href="https://www.cool2645.com/" class="pure-menu-link">2645
+                                    Studio</a></li>
+                            <li class="pure-menu-item"><a href="http://twitter.com/nanjolno/" class="pure-menu-link">Original
+                                    Twitter</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 
 @endsection
